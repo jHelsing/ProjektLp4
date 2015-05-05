4a70b598595606ac04467a8c328fa59d1ef30431
antontgu <antont@inbox.lv>
2015-05-05 23:09:11 +0200

    now supporting tests with junit

---
+++ core/src/test/java/dummyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+/**
+ * Created by anton on 2015-05-05.
+ */
+public class dummyTest {
+
+    @Test
+    public void dummy() {
+        assertEquals("dummy", "dummy");
+    }
+
+}
