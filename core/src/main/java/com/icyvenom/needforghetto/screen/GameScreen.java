f94d996617d8053ef566894a67f9ad1f0f6baa87
Marcus Aurén <auren@student.chalmers.se>
2015-05-09 11:19:11 +0200

    readded clear

--- core/src/main/java/com/icyvenom/needforghetto/screen/GameScreen.java
+++ core/src/main/java/com/icyvenom/needforghetto/screen/GameScreen.java
@@ -87,6 +87,10 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         // if we are in a paused state we dont update model
         switch (state) {
             case Running:
