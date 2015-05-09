61822dcb11b3dff283bd32fa2c4b0085df893fb1
Helsing <jonathan.helsing@gmail.com>
2015-05-09 15:22:11 +0200

    Some fixes

--- core/src/main/java/com/icyvenom/needforghetto/model/World.java
+++ core/src/main/java/com/icyvenom/needforghetto/model/World.java
@@ -37,6 +37,7 @@ public class World {
         //TODO add support for player vs. bullets and enemies vs. bullets
         if(enemies != null) {
             //If enemies exist we check collisions between player and enemies.
+
             //Creates a column and row where enemies might exist for crash to be possible.
             float minX = player.getBounds().getX();
             float maxX = player.getBounds().getWidth() + minX;
@@ -46,12 +47,11 @@ public class World {
             //Checking collisions between all enemies and the player
             for(int i = 0; i<enemies.size(); i++) {
                 Enemy e = enemies.get(i);
-                //Loops all the enemies on screen
+
                 float enemyMinX = e.getBounds().getX();
                 float enemyMaxX = e.getBounds().getWidth() + enemyMinX;
                 float enemyMinY = e.getBounds().getY();
                 float enemyMaxY = e.getBounds().getHeight() + enemyMinY;
-
 
                 if(minY <= enemyMinY && enemyMinY <= maxY) {
                     //Player coming from the bottom (3rd and 4th quadrant)
@@ -68,13 +68,14 @@ public class World {
                     if(minX <= enemyMaxX && enemyMaxX <= maxX) {
                         //Enemy is in the 2nd quadrant of a system with player as origin
                         enemies.remove(e);
-                    }
-                    if(minX <= enemyMinX && enemyMinX <= maxX) {
+                    } else if(minX <= enemyMinX && enemyMinX <= maxX) {
                         //Enemy is in the 1st quadrant of a system with player as origin
                         enemies.remove(e);
                     }
                 }
             }
         }
+
+        
     }
 }
