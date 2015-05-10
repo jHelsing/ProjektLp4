a3cc9c71552acf7285c553cfff9fdcd838b36157
Amaarillo <Amar_kulaglic@hotmail.com>
2015-05-11 00:10:36 +0200

    Enemy and Bullet are abstract now

---
+++ core/src/main/java/com/icyvenom/needforghetto/model/EnemySimple.java
@@ -0,0 +1,25 @@
+package com.icyvenom.needforghetto.model;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * This class is creating a specific enemy and contains basic
+ * functionality.
+ * @author Amar.
+ * @version 2.0
+ */
+public class EnemySimple extends Enemy {
+    /**
+     * The speed of the enemy.
+     */
+    private static final float SPEED=2f;
+
+    /**
+     *  When a enemy is created, it's position, the hitbox and the speed are set according to
+     *  the parameter and variables.
+     * @param position This is the given position for the enemy.
+     */
+    public EnemySimple(Vector2 position){
+        super(position, SPEED);
+    }
+}
