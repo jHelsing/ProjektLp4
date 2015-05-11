cc48b7d52626e02c3186ced0c5cdac25c34358ea
Amaarillo <Amar_kulaglic@hotmail.com>
2015-05-11 21:45:02 +0200

    BulletDirection, BulletNatoSeven and WeaponAWP are added

---
+++ core/src/main/java/com/icyvenom/needforghetto/model/BulletDirection.java
@@ -0,0 +1,20 @@
+package com.icyvenom.needforghetto.model;
+
+/**
+ * This Enum determine the direction of the object.
+ * @author Amar.
+ * @version 1.0
+ */
+public enum BulletDirection {
+    UP(1f), DOWN(-1f);
+
+    private final float directionValue;
+
+    BulletDirection(float directionValue) {
+        this.directionValue=directionValue;
+    }
+
+    public float getDirectionValue() {
+        return directionValue;
+    }
+}
---
+++ core/src/main/java/com/icyvenom/needforghetto/model/WeaponAWP.java
@@ -0,0 +1,41 @@
+package com.icyvenom.needforghetto.model;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * A class for a AWP sniper rifle with high damage and high attackrate. Can be used by
+ * both enemies and Players.
+ * @author Jonathan. Revisited by Amar.
+ * @version 1.0
+ */
+public class WeaponAWP extends Weapon {
+
+    private static final int WEAPON_DAMAGE = 3;
+
+    private static final float WEAPON_ATTACK_RATE = 3;
+
+    /**
+     * Constructor for a new SimpleWeapon
+     * @param position The position of the weapon, often the same position as the player/enemy.
+     *                 This will be the position of any new bullet that are added to the bullet list
+     */
+    public WeaponAWP(Vector2 position) {
+        super.setPosition(position);
+        super.setAttackRate(WEAPON_ATTACK_RATE);
+        super.setDamage(WEAPON_DAMAGE);
+    }
+
+    public WeaponAWP(BulletDirection bulletDirection) {
+        super(bulletDirection);
+        super.setAttackRate(WEAPON_ATTACK_RATE);
+        super.setDamage(WEAPON_DAMAGE);
+    }
+
+    /**
+     * Used to add a bullets to the bullet-list
+     */
+    public void addBullet(){
+        super.getBullets().add(new BulletNatoSeven(super.getPosition().cpy(), super.getBulletDirection()));
+    }
+
+}
